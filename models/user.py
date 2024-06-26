86886fcba0b8273b6ec2a7f2855cc9fde972804e
NickelannG <8836@holbertonstudents.com>
2024-06-26 12:00:08 +1000

    updated reviews model file

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ import uuid
 import re
 from flask import jsonify, request, abort
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import relationship
 from data import storage, USE_DB_STORAGE, Base
 
 class User(Base):
@@ -32,7 +33,7 @@ class User(Base):
         __email = Column("email", String(128), nullable=False)
         __password = Column("password", String(128), nullable=False)
         # properties = relationship("Place", back_populates="owner", cascade="delete, delete-orphan")
-        # reviews = relationship("Review", back_populates="writer", cascade="delete, delete-orphan")
+        reviews = relationship("Review", back_populates="writer", cascade="delete, delete-orphan")
 
     # Constructor
     def __init__(self, *args, **kwargs):
